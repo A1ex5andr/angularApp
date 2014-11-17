fed123e73bfbde0c744a196c28f98f4ab1cf206e
Megavolt <atsybko@reaktivate.com>
2014-11-17 15:06:28 +0200

    1

--- source/modules/cart/js/cart.js
+++ source/modules/cart/js/cart.js
@@ -3,12 +3,18 @@
     var app = angular.module('cart', []);
 
     app.controller('CartCtrl', function ($scope, productService) {
-
             $scope.products = productService.getProducts();
             this.products = $scope.products;
             this.qty = 1;
 
             console.log(this.products.length);
 
+            this.getCount = function() {
+                return this.products.length;
+            };
+
+            this.removeItem = function(index) {
+                productService.removeProduct(index);
+            };
         });
 }());
